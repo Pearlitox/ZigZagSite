aead093b6f5dc6689906df6d426211315bc5955c
Julie <ducarnejulie05@gmail.com>
2024-10-18 12:40:39 +0200

    change seo

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -55,12 +55,93 @@ glowInTexts.forEach(glowInText => {
 //Rewrite 
 // ——————————————————————————————————————————————————
 
+// class TextScramble {
+//   constructor(el) {
+//     this.el = el;
+//     this.chars = '!<>-_\\/[]{}—=+*^?#________';
+//     this.update = this.update.bind(this);
+//   }
+//   setText(newText) {
+//     const oldText = this.el.innerText;
+//     const length = Math.max(oldText.length, newText.length);
+//     const promise = new Promise((resolve) => this.resolve = resolve);
+//     this.queue = [];
+
+//     for (let i = 0; i < length; i++) {
+//       const from = oldText[i] || '';
+//       const to = newText[i] || '';
+//       const start = Math.floor(Math.random() * 40);
+//       const end = start + Math.floor(Math.random() * 40);
+//       this.queue.push({ from, to, start, end });
+//     };
+
+//       cancelAnimationFrame(this.frameRequest)
+//       this.frame = 0;
+//       this.update();
+//       return promise;
+
+//   };
+
+
+//   update() {
+//     let output = '';
+//     let complete = 0;
+
+//     for (let i = 0, n = this.queue.length; i < n; i++) {
+//       let { from, to, start, end, char } = this.queue[i];
+
+//       if (this.frame >= end) {
+//       complete++
+//       output += to
+//       } else if (this.frame >= start) {
+//         if (!char || Math.random() < 0.28) {
+//             char = this.randomChar()
+//             this.queue[i].char = char
+//         }
+//       output += `<span class="dud">${char}</span>`
+//       } else {
+//         output += from
+//       }
+//     };
+//     this.el.innerHTML = output;
+//     if (complete === this.queue.length) {
+//         this.resolve()
+//     } else {
+//         this.frameRequest = requestAnimationFrame(this.update)
+//         this.frame++
+//     };
+//   };
+//   randomChar() {
+//     return this.chars[Math.floor(Math.random() * this.chars.length)]
+//   }
+// };
+
+
+// const phrases = [
+//   "Mais d'abord...,",
+//   "C'est quoi le TDAH ?" 
+// ];
+
+// const el = document.querySelector('.whatis__text');
+// const fx = new TextScramble(el);
+
+// let counter = 0
+// const next = () => {
+//   fx.setText(phrases[counter]).then(() => {
+//     setTimeout(next, 800);
+//   })
+//   counter = (counter + 1) % phrases.length;
+// };
+
+// next();
+
 class TextScramble {
   constructor(el) {
     this.el = el;
     this.chars = '!<>-_\\/[]{}—=+*^?#________';
     this.update = this.update.bind(this);
   }
+
   setText(newText) {
     const oldText = this.el.innerText;
     const length = Math.max(oldText.length, newText.length);
@@ -73,15 +154,13 @@ class TextScramble {
       const start = Math.floor(Math.random() * 40);
       const end = start + Math.floor(Math.random() * 40);
       this.queue.push({ from, to, start, end });
-    };
+    }
 
-      cancelAnimationFrame(this.frameRequest)
-      this.frame = 0;
-      this.update();
-      return promise;
-
-  };
-
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
 
   update() {
     let output = '';
@@ -91,49 +170,58 @@ class TextScramble {
       let { from, to, start, end, char } = this.queue[i];
 
       if (this.frame >= end) {
-      complete++
-      output += to
+        complete++;
+        output += to;
       } else if (this.frame >= start) {
         if (!char || Math.random() < 0.28) {
-            char = this.randomChar()
-            this.queue[i].char = char
+          char = this.randomChar();
+          this.queue[i].char = char;
         }
-      output += `<span class="dud">${char}</span>`
+        output += `<span class="dud">${char}</span>`;
       } else {
-        output += from
+        output += from;
       }
-    };
+    }
+
     this.el.innerHTML = output;
     if (complete === this.queue.length) {
-        this.resolve()
+      this.resolve();
     } else {
-        this.frameRequest = requestAnimationFrame(this.update)
-        this.frame++
-    };
-  };
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)]
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
   }
-};
 
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
 
-const phrases = [
-  "Mais d'abord...,",
-  "C'est quoi le TDAH ?" 
+// Liste de phrases spécifiques pour chaque élément
+const phrasesList = [
+  ["Introduction", "Le TDAH"],       // Phrases pour le deuxième titre
+  ["Mais d'abord...", "C'est quoi le TDAH ?"],  // Phrases pour le premier titre
+  ["Origine du projet", "Nos recherches"],       // Phrases pour le deuxième titre
+  // Ajoute d'autres listes de phrases pour d'autres éléments
 ];
 
-const el = document.querySelector('.whatis__text');
-const fx = new TextScramble(el);
+// Sélectionne tous les éléments ayant la classe 'scramble-text'
+const elements = document.querySelectorAll('.titleAnim');
 
-let counter = 0
-const next = () => {
-  fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 800);
-  })
-  counter = (counter + 1) % phrases.length;
-};
+elements.forEach((el, index) => {
+  const fx = new TextScramble(el);
+  const phrases = phrasesList[index];  // Utilise les phrases spécifiques pour cet élément
 
-next();
+  let counter = 0;
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 800);
+    });
+    counter = (counter + 1) % phrases.length;
+  };
+
+  next();
+});
 
 
 
